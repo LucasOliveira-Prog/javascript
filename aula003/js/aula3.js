34831ff28923ed4dc0494869685c0079ad7903f7
Lucas <lucasoliveira.prog7@gmail.com>
2026-09-12 17:54:14 -0300

    complementar

---
+++ aula003/js/aula3.js
@@ -0,0 +1,35 @@
+document.write('Olá mundo!')
+console.log('Outro olá mundo')
+
+let mensagem = "Minha mensagem"
+let mensagem2 = "Outra mensagem"
+let meuPeso = 83.5
+let minhaAltura = 1.75
+let minhaIdade = 40
+let ehDoador = false
+let teste = null
+
+/* criar constantes e atribuir valor */
+const PI = 3.1415
+const TAXA = 0.5
+
+teste = 'Algum conteúdo'
+
+/* escrever o conteudo de variaveis e constantes */
+document.write(`<p>${mensagem}</p>`)
+
+document.write(mensagem2)
+
+document.write(`<p>${meuPeso}</p>`)
+
+document.write(minhaAltura)
+
+document.write(`<p>${minhaIdade}</p>`)
+
+document.write(ehDoador)
+
+document.write(`<p>${teste}</p>`)
+
+document.write(PI)
+
+document.write(`<p>${TAXA}</p>`)
