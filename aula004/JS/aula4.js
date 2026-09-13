15c19aef00a24432b9457859686c955e1ea2a7a0
Lucas <lucasoliveira.prog7@gmail.com>
2026-09-13 12:39:19 -0300

    operadores e operações

---
+++ aula004/JS/aula4.js
@@ -0,0 +1,22 @@
+let n1 = 10
+let n2 = 2
+let n3 = "2"
+
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % n2)
+
+console.log(n2 == n3) // igual a
+console.log(n2 === n3) // identico a
+console.log(n2 != n3) // diferente de ou Nao=
+console.log(n2 !== n3) // nao e identico
+
+let a = true
+let b = false
+
+console.log(a && b) // e/and
+console.log(a || b) // ou/or
+console.log(!a) // not/nao
+console.log(!b)
